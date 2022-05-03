552de7a02c2e22670f7f9ff0dd8bc60ff807e65b
zerontec <zerontecnologia1@gmail.com>
2022-05-03 00:30:51 -0430

    gitignore indexjs package

---
+++ index.js
@@ -0,0 +1,39 @@
+//                o88o            o88o
+//               o8888o          o8888o
+//                 o888888oo0oo888888o
+//                     8o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//      go go you can do     
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+
+
+// Syncing all the models at once.
+conn.sync({ force: false }).then(() => {
+    server.listen(port, () => {
+      console.log('o|O_O|o robot Σωκράτης listening at 5040');
+      // eslint-disable-line no-console
+     /*  defaultAdminAndRoles();
+      ratingInitial(); */
+    });
+  /*   getTypes(); // <<<--- para iniciar la base de datos
+    getApiToDb(); */
+  });
